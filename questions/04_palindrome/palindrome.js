861b60010c7c86ce15539dd994994ce0006489e2
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-02 09:12:42 -0400

    palindrome sol

--- questions/04_palindrome/palindrome.js
+++ questions/04_palindrome/palindrome.js
@@ -1,8 +1,25 @@
 /* Given a string, determine if it is a palindrome */
 /* A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam, or racecar */
 
-const validPalindrome = () => {
-  //YOUR CODE
+const validPalindrome = (str) => {
+  let len="";
+  if(typeof str == 'string'){
+    len  = str.length;
+  }else{
+      len = str.toString().length;
+  }
+  //console.log("len == "+len);
+  //let len = str.length;
+  let isPalindrome = false;
+  
+ for(let i = 0;i<=len/2;i++){ //abba // 4 
+    if(str[i] != str[len-1-i]){
+      isPalindrome = false;
+    }else{
+      isPalindrome = true;
+    }
+ }
+ return isPalindrome;
 };
 
 module.exports = { validPalindrome };
