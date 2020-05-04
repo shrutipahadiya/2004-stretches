bb4c69b6294aa3c7d676f06699c999251a8fef0a
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-04 18:57:16 -0400

    lockbox

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,4 +1,28 @@
-const lockBox = () => {
+const lockBox = (code,message) => {
+      
+    if(typeof code != 'number' || typeof message != 'string'){
+    throw "incorrect type for code or message";
+
+}
+
+
+modCode =(oldCode,newCode) => {
+  
+
+}
+
+modMessage = ()  =>{
+
+
+}
+
+
+revealMessage = () => {
+
+
+}
+
+
   // write code here
 };
 
