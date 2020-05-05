5fb8a32a5fe1ecc5a43c1fdff1383658e637c57a
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-05 11:38:40 -0400

    06_zip

--- questions/06_zip/zip.js
+++ questions/06_zip/zip.js
@@ -8,7 +8,35 @@ zip([
 */
 
 function zip(objs) {
-  // YOUR CODE
+  let result = {};
+  for (let i = 0; i < objs.length; i++) { //4
+    let keys = Object.keys(objs[i]);  //x
+    //console.log(keys);
+    //console.log(objs[i].length);
+    if (keys.length > 0) {
+      //console.log(keys.length);
+      for (let j = 0; j < keys.length; j++) {
+
+        let key = keys[j];
+        // console.log(key);
+        // console.log(objs[i][key]);
+
+        if (result[key] == undefined) {
+          // console.log("inside undefined");
+          result[key] = objs[i][key];
+          //console.log( result[key]);
+        } else {
+          result[key] = result[key] + objs[i][key];
+        }
+      }
+    }
+
+  }
+
+  //console.log(result);
+  return result;
 }
+
+
 
 module.exports = { zip };
