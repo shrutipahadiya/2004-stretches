3a0b1d4ba0572419f1663badaa5d919c48999326
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-07 10:40:23 -0400

    trim sol

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -2,6 +2,17 @@
 
 const trim = obj => {
   // YOUR CODE
+  let newObj = {};
+  let keys = Object.keys(obj);
+ keys.forEach(element => {
+   //console.log("element is ",element);
+   if(obj[element] != undefined || obj[element] != null){
+      newObj[element] = obj[element];
+   }
+ });
+
+ //console.log(newObj);
+  return newObj;
 };
 
 module.exports = { trim };
