08f5c795b51b2b31c50a66c0e1031932c7ad385d
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-08 11:58:28 -0400

    diceRoller sol

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -4,6 +4,38 @@
 
 class DiceRoller {
   //YOUR CODE
+  constructor(noOfSides,noOfDice){
+
+    if(noOfSides < 0 || noOfDice == 0 || typeof noOfSides == 'string' || 
+      typeof noOfDice == 'string' || noOfSides == undefined || noOfDice == undefined){
+          throw "invalid number of sides or total dice ";
+    }
+    this.noOfSides = noOfSides;
+    this.noOfDice = noOfDice;
+    this.history = [];
+    
+
+  }
+  
+  randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+roll(){
+   let diceRoll = [];
+for(let i=0;i<this.noOfDice;i++){
+    let rollValue = this.randomInteger(1,this.noOfSides);
+      diceRoll.push(rollValue);
+    
+}
+//console.log(diceRoll);
+this.history.push(diceRoll);
+return diceRoll;
+
+}
+
+
 }
 
 module.exports = { DiceRoller };
