edceeab38b0d45eaeb9da51eaaa20d5cf9e8e90e
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-09 13:01:11 -0400

    test commit

--- questions/10_splatter/splatter.js
+++ questions/10_splatter/splatter.js
@@ -2,6 +2,17 @@
 
 const splatter = () => {
   //write code here
+  console.log("length ",arguments.length);
+  let sum = 0;
+  for(let i=0;i<arguments.length;i++){
+    let keys = Object.keys(arguments[i]);
+    console.log("keys ",keys);
+    for(let j=0;j<keys.length;j++){
+      sum = sum+arguments[i][j];
+    }
+  }
+  console.log(sum);
+  return sum;
 };
 
-module.exports = { splatter };
+module.exports = { splatter }; 
