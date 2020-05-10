25502bed19ce8e88ebe4693bb60d7fbc2fe6c43f
Deanna Wong <dwong221@gmail.com>
2020-05-10 10:11:28 -0400

    romanInteger stretch

---
+++ questions/11_romanInteger/romanInteger.js
@@ -0,0 +1,29 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+/*
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+I can be placed before V (5) and X (10) to make 4 and 9.  
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+*/
+/*
+examples:
+1 // I
+2 // II
+3 // III
+4 // IV
+1994 // MCMXCIV
+*/
+
+const romanInteger = () => {
+  //YOUR CODE HERE
+};
+
+module.exports = { romanInteger };
---
+++ questions/11_romanInteger/romanInteger.spec.js
@@ -0,0 +1,23 @@
+const { romanInteger } = require('./romanInteger.js');
+
+describe('romanInteger', () => {
+  test('It should take a number and a return a string', () => {
+    const integerToRoman = romanInteger(5);
+    expect(typeof integerToRoman).toEqual('string');
+  });
+  test('It should correctly convert an integer into Roman numerals', () => {
+    expect(romanInteger(3)).toEqual('III');
+    expect(romanInteger(20)).toEqual('XX');
+    expect(romanInteger(50)).toEqual('L');
+    expect(romanInteger(400)).toEqual('CD');
+    expect(romanInteger(444)).toEqual('CDXLIV');
+    expect(romanInteger(1111)).toEqual('MCXI');
+    expect(romanInteger(1999)).toEqual('MCMXCIX');
+    expect(romanInteger(4999)).toEqual('MMMMCMXCIX');
+  });
+  test('It should throw an error if given an invalid number', () => {
+    expect(() => romanInteger(-1)).toThrow();
+    expect(() => romanInteger(0)).toThrow();
+    expect(() => romanInteger(5000)).toThrow();
+  });
+});
