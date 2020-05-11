29afa30fdae5742c05186deadb1c518a59099fa2
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-11 18:50:12 -0400

    fizzBuzz

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,22 @@
-const fizzBuzz = () => {
+const fizzBuzz = (len) => {
   //YOUR CODE HERE
+  let arr = [];
+  if(typeof len == 'string' || typeof len < 0 || typeof len != 'number'){
+    throw "invalid input"
+  }else{
+    for(let i=0;i<len;i++){
+    if(i % 3 == 0 && i%5 == 0 ){
+
+      arr.push("FizzBuzz")
+  }else if(i % 3 == 0){
+      arr.push("Fizz")
+    }else if(i % 5 == 0 ){
+      arr.push("Buzz")
+    }else{
+      arr.push(i)
+    }
+  }
+}
+  return arr;
 };
 module.exports = { fizzBuzz };
