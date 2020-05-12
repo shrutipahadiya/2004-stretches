4208a945461ede54cca166af2989799c40a8ea1d
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-12 11:53:04 -0400

    comparator sol

--- questions/13_comparator/comparator.js
+++ questions/13_comparator/comparator.js
@@ -1,5 +1,32 @@
 //write a func that takes two arg... selectKey and obj
 
-const comparator = () => {};
+const comparator = (selectKey, obj) => {
+   
+        if (selectKey == 'age') {
+            obj.sort(function (a, b) {
+                return a.age - b.age;
+            });
+
+        } else if (selectKey == 'name') {
+          //  console.log("for name key case ===")
+            obj.sort(function (a, b) {
+                let ln1 =  a.name.split(" ")[1];
+                let ln2 = b.name.split(" ")[1];
+                if (ln1 < ln2) {
+                    return -1;
+                  }
+                  if (ln1 > ln2) {
+                    return 1;
+                  }
+                   return 0;
+                
+            });
+        }else{
+            if (!obj.hasOwnProperty(selectKey)) {
+                return "incorrect key";
+            } 
+        }
+       return obj;
+    };
 
 module.exports = { comparator };
