9731b203061d2662def38d6d5c7e46bcdb6febc0
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-13 18:47:32 -0400

    sol

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,8 +7,23 @@
 // * Play *
 // ********
 
-const movieposter = () => {
+const movieposter = (arr) => {
   //write code here
+  let ans = "***********\n";
+  if(arr.length <=0){
+    throw "invalid length";
+  }else{
+    for(let i=0;i<arr.length;i++){
+        ans = ans+"* ";
+        ans = ans+arr[i];
+        ans = ans+" *\n";
+       if(i == arr.length-1) {
+         ans = ans+"***********"
+       }
+    }
+  }
+  console.log(ans)
+  return ans;
 };
 
 module.exports = { movieposter };
