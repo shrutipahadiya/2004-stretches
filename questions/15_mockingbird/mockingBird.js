a321c4521f2a939c02b576fd5f95f98ef47e8170
Deanna Wong <dwong221@gmail.com>
2020-05-13 20:22:42 -0400

    added 15 mockingbird

---
+++ questions/15_mockingbird/mockingBird.js
@@ -0,0 +1,8 @@
+// repeater runs a function n times and returns an array of its outputs
+const repeater = (func, n) => {
+  // YOUR CODE
+};
+
+// see test specs to complete this stretch
+
+module.exports = { repeater };
