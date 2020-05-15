e19d8cafabec0b0e3316ee001695e902bf7cade8
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-15 16:00:05 -0400

    sol

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,22 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+  
+    let num =1;
+    let ans ="";
+    for(let i=1;i<=n;i++){ //3
+                for(let j=1;j<=i;j++){
+             ans = ans+num
+             ans = ans+'\n' 
+               // console.log(num)
+                num ++
+             }  
+                
+ }
+ console.log(ans);
+   
+    return ans;
+ };
 
 module.exports = { floydsTriangle };
