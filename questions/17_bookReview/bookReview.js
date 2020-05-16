35aeed17c90a6bbcf786fa6ddcdf37db0b05b618
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-16 09:09:01 -0400

    sol

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,31 @@
-class BookReview {}
+class BookReview {
+
+    constructor(title,author){
+            this.title = title;
+            this.author = author;
+            this.rating = 1;
+            this.pageNo =1;
+            this.notes =[];
+    }
+    
+
+   addNotes(pageNo,notes){
+       if( this.notes[pageNo]  == undefined ){
+        this.notes[pageNo] = notes;
+       }else{
+        this.notes[pageNo] = this.notes[pageNo] +" "+ notes;
+       }
+      
+   }     
+
+
+    addRating(rating){
+        if(rating >5 ){
+            throw "rating cannot be greater than 5"
+        }else{
+                this.rating = rating;
+        }
+    }
+
+}
 module.exports = { BookReview };
