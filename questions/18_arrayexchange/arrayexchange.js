fe3d4295970a0c115b60a0e8980f1ead11bc6aa6
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-17 19:31:03 -0400

    sol

--- questions/18_arrayexchange/arrayexchange.js
+++ questions/18_arrayexchange/arrayexchange.js
@@ -1,3 +1,6 @@
+//arrayexchange should exchange the elements of two arrays in-place in a way that their new content is also reversed.
+//restricted use: reverse()
+
 const arrayexchange = (arr1, arr2) => {
 
     if (Array.isArray(arr1) == false || Array.isArray(arr2) == false) {
@@ -9,17 +12,17 @@ const arrayexchange = (arr1, arr2) => {
         for (let i = 0; i < length1; i++) {
             newArr.push(arr1.pop());
         }
-       // console.log("newArr ", newArr);
+        //console.log("newArr ", newArr);
         for (let i = 0; i < length2; i++) {
             arr1.push(arr2.pop());
         }
-      //  console.log("arr1 ", arr1);
+        //console.log("arr1 ", arr1);
         for (let i = 0; i < length1; i++) {
             arr2.push(newArr.shift());
         }
-      //  console.log("arr2 ", arr2);
-       // console.log("arr1", arr1)
-     //  console.log("arr2", arr2)
+        //console.log("arr2 ", arr2);
+        //console.log("arr1", arr1)
+        //console.log("arr2", arr2)
     }
 };
 
