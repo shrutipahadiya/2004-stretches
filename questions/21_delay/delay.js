c8187b17f9a77f7822de5933f67bfd09e4565545
Deanna Wong <dwong221@gmail.com>
2020-05-20 18:13:11 -0400

    delay Stretch

--- questions/21_delay/delay.js
+++ questions/21_delay/delay.js
@@ -1,9 +1,5 @@
 const delay = (str, time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(str);
-    }, time);
-  });
+  //Your code here
 };
 
 module.exports = { delay };
