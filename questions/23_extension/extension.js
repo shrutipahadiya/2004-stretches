aff5250db60797e1bd983c0147dae92607d8b71e
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-23 21:20:19 -0400

    extension

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,20 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (func,a,b) => {
+    console.log(typeof func)
+    // console.log(typeof a)
+    // console.log(typeof b)
+    
+
+   if(typeof func != 'function'){
+        throw "func should be a function"
+
+   }else{
+        return func(a,b)  
+   }
+  
+
+
+};
 
 module.exports = { extensions };
