1b9d610b39a7b1db7dd1ec442e720f92ed561cef
Deanna Wong <dwong221@gmail.com>
2020-05-26 09:05:45 -0400

    RPS

---
+++ questions/26_rps/rps.js
@@ -0,0 +1,13 @@
+//You are writing the game rock-paper-scissors.
+/* 
+RULES:
+Scissors defeat Paper
+Paper defeats Rock
+Rock defeats Scissors 
+*/
+
+class RPS {
+  //YOUR CODE HERE
+}
+
+module.exports = { RPS };
