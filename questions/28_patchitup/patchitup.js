f6fd63f5a81a3a32f2bcac4cc05eede1b7d7e8ea
Thompson Harris <william.thompson.harris@gmail.com>
2020-05-29 10:10:54 -0400

    added new patch test

---
+++ questions/28_patchitup/patchitup.js
@@ -0,0 +1,9 @@
+// - "monkey-patching" means changing the behavior of built-in code at runtime.
+// - your job is to monkey-patch the Array class, by adding a method called "countBy"
+// - countBy should optionally take a function argument
+// - countBy returns an object whose keys correspond to the elements in the array it is called on, and whose values correspond to the number of times that element appears in the array
+// - when countBy is passed a function argument "fn", the values correspond to the number of times that key was returned by fn.
+// - NB: monkey-patching is generally not good, do not do it, this is just an exercise
+// - Also NB: repl.it doesn't let you monkey patch! but the chrome console does.
+
+//code goes here
