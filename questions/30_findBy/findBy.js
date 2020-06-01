bd1572b64eeb031765d162f20a1fb85a0a125b4f
Shruti Pahadiya <15.shruti@gmail.com>
2020-05-31 20:10:14 -0400

    findBy stretch

--- questions/30_findBy/findBy.js
+++ questions/30_findBy/findBy.js
@@ -1,5 +1,8 @@
-const findBy = () => {
+const findBy = (arr,key,value) => {
   //YOUR CODE HERE
+const found = arr.find(obj => obj.name==value || obj.type==value);
+//console.log("found ",found);
+return found;
 };
 
 module.exports = { findBy };
