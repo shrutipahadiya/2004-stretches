49efd22829b504a02489a1a43d2c7b49fc99bdcc
Shruti Pahadiya <15.shruti@gmail.com>
2020-06-03 18:56:30 -0400

    sol

--- questions/33_directory/directory.js
+++ questions/33_directory/directory.js
@@ -10,8 +10,23 @@
 // };
 // directory(phonebookData)=>{ 'a/b/c': 12 }
 
-const directory = () => {
+const directory = (obj) => {
   //code in here
+ 
+  let ans ={};
+  let str="";
+ let keys = Object.keys(obj);
+ console.log(keys);
+ for(let i=0;i<keys.length;i++){
+    if(obj[i] instanceof Object){
+        str = str+obj[i]+"/";
+    }else{
+      ans[str] = obj[i];
+    }
+ }
+ console.log("str is = ",str);
+ console.log("ans is = ",ans);
+  return ans;
 };
 
 module.exports = { directory };
