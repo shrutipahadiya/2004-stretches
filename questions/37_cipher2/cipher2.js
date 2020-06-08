f243995a5de61e08707bd57a7245dd0877ead24e
Shruti Pahadiya <15.shruti@gmail.com>
2020-06-08 18:54:29 -0400

    cipher2

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,17 @@
-const decypher = () => {
+const decypher = (str) => {
   //YOUR CODE HERE
+  let ans = "";
+  let arr=[];
+  for(let i=0;i<str.length;i++){
+   // console.log("1111111 ",typeof str[i]);
+    if(typeof parseInt(str[i]) == 'number'){
+     // console.log("22222222222")
+      arr.push(str[i])
+    }
+  }
+  console.log("arr ",arr)
+  ans = String.fromCharCode(arr)
+  console.log("ans ",ans)
+  return ans;
 };
 module.exports = { decypher };
