757f02c3215596efd63b910a195aa102e0c2bcda
Shruti Pahadiya <15.shruti@gmail.com>
2020-06-11 20:17:30 -0400

    stretch

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,17 @@
 //YOUR CODE HERE
+class Box{
+    constructor(color,capacity){
+        this.color = color;
+        this.capacity = capacity;
+        this.contents=[];
+    }
+  
 
+
+pack(color,capacity){
+    this.contents.push(new Box(color,capacity))
+
+}
+
+}
 module.exports = { Box };
