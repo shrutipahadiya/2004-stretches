1a79f208da07acb47d110d2900e43846b5a8bc5e
Thompson Harris <william.thompson.harris@gmail.com>
2020-06-14 10:32:32 -0400

    added makeyourown

---
+++ questions/43_makeyourown/makeyourown.js
@@ -0,0 +1,11 @@
+// Make your own stretch
+// try to solve for a challenging problem
+// write at minium three test specs
+// Jest documentation: https://github.com/sapegin/jest-cheat-sheet
+// submit the stretch with solution code and test specs passing
+
+const makeyourown = () => {
+  console.log('make your own stretch!');
+};
+
+module.exports = { makeyourown };
---
+++ questions/43_makeyourown/makeyourown.spec.js
@@ -0,0 +1,7 @@
+const { makeyourown } = require('./makeyourown');
+
+describe('makeyourown functionality', () => {
+  it('your first test spec', () => {
+    expect(makeyourown()).toEqual();
+  });
+});
