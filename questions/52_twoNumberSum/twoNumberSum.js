039de5edb6e7d105b91d2e6c963437e12a76f269
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-11 09:19:34 -0400

    stretch

--- questions/52_twoNumberSum/twoNumberSum.js
+++ questions/52_twoNumberSum/twoNumberSum.js
@@ -5,8 +5,25 @@
 
 //***Your solution should NOT include nested loops of any type***
 
-const twoNumberSum = () => {
+//expect(twoNumberSum([5, -12, -6, 12, 5, 89, 9, -3], 83)).toEqual([-6, 89]);
+
+const twoNumberSum = (arr,sum) => {
   //write code here
+  let temp ;
+  let newarr=[];
+//let i=0;
+ 
+  for(let i=0;i<arr.length;i++){
+      if(sum-arr[i] == arr[i+1] && i<arr.length){
+        newarr.push(arr[i]);
+        newarr.push(arr[i+1]);
+        break;
+      }else{
+        continue;
+      }
+  }
+  console.log(newarr);
+  return newarr;
 };
 
 module.exports = { twoNumberSum };
