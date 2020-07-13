45ca5aafb77fe63c3e5629b445a605038b9187de
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-13 18:56:01 -0400

    stretch

--- questions/53_balancedArr/balancedArr.js
+++ questions/53_balancedArr/balancedArr.js
@@ -1,5 +1,20 @@
-const balanced = () => {
+const balanced = (arr) => {
   //YOUR CODE HERE
+  let sum=0;
+  let index=0;
+  for(let i=0;i<arr.length;i++){
+    if(i > 0 || i<arr.length-1){
+    for(let j=i+1;j<arr.length;j++){
+       sum = sum+arr[j];
+    }
+    if(arr[i] == sum){
+      index = i+1;
+    }
+
+  }
+}
+console.log(index);
+return index;
 };
 
 module.exports = balanced;
