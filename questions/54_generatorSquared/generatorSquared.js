56370f7280f5f52455518ce8e6fbc03aa65475a5
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-15 18:50:59 -0400

    stretch

--- questions/54_generatorSquared/generatorSquared.js
+++ questions/54_generatorSquared/generatorSquared.js
@@ -3,5 +3,21 @@
 // Pass the specs provided
 
 //write your function here
+const sqauredGen=(a,b)=>{
+    let ans=1;
+    let arr=[];
+   
+    for(let i=a+1;i<=b;i++){
+        ans=i*i;
+        arr.push(ans);
+    }
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] <= b ){
+            return arr[i];
+        }else{
+            return true;
+        }
+    }
 
+}
 module.exports = { sqauredGen };
